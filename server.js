a8230fe10d3021c13d66c78067c169712e8cf608
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2019-02-05 06:14:48 -0700

    Get server to return correct results from scrape. Then store to DB, then render, then make page pretty

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ var db = require("./models");
 // Connection
 var PORT = 3000;
 
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+var app = express();
+
+    app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static("public"));  <---- Do I need this??
 
@@ -23,3 +24,34 @@ app.set("view engine", "handlebars");
 
 mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
 
+
+// Route
+
+app.get("/scrape", function(req, res) {
+    axios.get("https://www.goodnewsnetwork.org/category/news/").then(function(response) {
+        var $ = cheerio.load(response.data);
+
+    $("h3.entry-title").each(function(i, element) {
+
+        var result = {};
+
+        var title = $(element).text();
+        var link = $(element).children().attr("href");
+
+        result.headline = $(this).children("a").text();
+        result.link = $(this).children("a").attr("href");
+            // Need a summary section too.
+            // Image URL would be nice as well.
+        
+
+    })
+    console.log(result);
+})
+});
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
